582e74c67744bc621985c7b56101b85170756b4c
Json <98847798+JeissonSGG@users.noreply.github.com>
2026-02-27 21:20:20 -0500

    Add com complejidad eje#4

--- Parcial#1/Ejercicio 4/Stones.java
+++ Parcial#1/Ejercicio 4/Stones.java
@@ -1,5 +1,16 @@
 import java.util.*;
-// Fuerza Bruta
+/*
+* Fuerza Bruta
+* Complejidad:
+* Tiempo: O(n)
+* Aunque hay dos for, el segundo realmente solo se ejecuta una vez
+* por cada iteración del primero (j = i + 1). Por lo tanto,
+* el número total de comparaciones es n - 1.
+*
+* Espacio: O(1)
+* Solo se usan variables primitivas (count, n, etc.),
+* no se crean estructuras adicionales dependientes de n.
+*/
 public class Stones {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -20,7 +31,17 @@ public class Stones {
     }
 }
 
-//Optimizado
+/*
+* Optimizado
+* Complejidad:
+* Tiempo: O(n)
+* Se recorre la cadena una sola vez comparando cada
+* carácter con el anterior.
+* 
+* Espacio: O(1)
+* Solo se utiliza una variable auxiliar (z)
+* y un contador. No depende del tamaño de entrada.
+*/
 public class Stones{
     public static void main(String[] arg){
         Scanner sc = new Scanner(System.in);
@@ -38,4 +59,5 @@ public class Stones{
         System.out.println(count);
     }
 }
+
 
