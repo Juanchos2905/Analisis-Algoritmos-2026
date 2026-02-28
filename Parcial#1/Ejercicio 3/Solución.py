3621310548f9aa7c59d4e3142f71f01e4a0a9f0c
jcardser <100744945+jcardser@users.noreply.github.com>
2026-02-27 21:52:37 -0500

    Implement maxProfit method in Solution class
    Added a class structure and defined maxProfit method to calculate maximum profit from stock prices.

--- Parcial#1/Ejercicio 3/Solución.py
+++ Parcial#1/Ejercicio 3/Solución.py
@@ -1,1 +1,17 @@
+class Solution:
+        #Complejidad Temporal: O(n) - Itera una sola vez por la lista.
+        #Complejidad Espacial: O(1) - Usa memoria constante para las variables.
+    def maxProfit(self, prices: list[int]) -> int:
+        
+        min_price = float('inf')
+        max_profit = 0
+        
+        for price in prices:
 
+            if price < min_price:
+                min_price = price
+  
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+                
+        return max_profit
