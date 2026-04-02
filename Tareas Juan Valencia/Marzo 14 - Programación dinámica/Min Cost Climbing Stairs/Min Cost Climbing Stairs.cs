1cea041b1c3ca6d1ea2efab5444838141280cd83
Juanchos2905 <juanchos2905@yopmail.com>
2026-04-02 16:42:13 -0500

    Min Cost Climbing Stairs

---
+++ Tareas Juan Valencia/Marzo 14 - Programación dinámica/Min Cost Climbing Stairs/Min Cost Climbing Stairs.cs
@@ -0,0 +1,47 @@
+public class Solution
+{
+    public int MinCostClimbingStairs(int[] cost)
+    {
+        /***
+            Subproblema:
+            Haremos costoMinimoHastaEscalon[i] que nos va a indicar
+            el costo mínimo necesario para llegar exactamente hasta el escalón i.
+
+            Recurrencia:
+            costoMinimoHastaEscalon[i] =
+                cost[i] + min(
+                    costoMinimoHastaEscalon[i-1],
+                    costoMinimoHastaEscalon[i-2]
+                )
+
+            El resultado final es el mínimo entre:
+                costoMinimoHastaEscalon[n-1]
+                costoMinimoHastaEscalon[n-2]
+
+            Complejidad:
+            Tiempo: O(n)
+            Espacio: O(n)
+        ***/
+
+        int cantidadEscalones = cost.Length;
+
+        int[] costoMinimoHastaEscalon = new int[cantidadEscalones]; //dp
+        costoMinimoHastaEscalon[0] = cost[0];
+        costoMinimoHastaEscalon[1] = cost[1];
+
+        for (int indiceActual = 2; indiceActual < cantidadEscalones; indiceActual++)
+        {
+            costoMinimoHastaEscalon[indiceActual] =
+                cost[indiceActual] +
+                Math.Min(
+                    costoMinimoHastaEscalon[indiceActual - 1],
+                    costoMinimoHastaEscalon[indiceActual - 2]
+                );
+        }
+
+        return Math.Min(
+            costoMinimoHastaEscalon[cantidadEscalones - 1],
+            costoMinimoHastaEscalon[cantidadEscalones - 2]
+        );
+    }
+}
